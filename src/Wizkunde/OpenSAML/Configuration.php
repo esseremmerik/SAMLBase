05d0827a7e32378f0cd2b3e0d651399c1d191663
Ron van der Molen <ron@wizkunde.nl>
2013-07-18 22:00:39 +0200

    [FEATURE] Added a configuration object that will hold the configuration data

---
+++ src/Wizkunde/OpenSAML/Configuration.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace Wizkunde\OpenSAML;
+
+use Wizkunde\OpenSAML\Configuration\Certificate;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * @var array Basic configuration
+     */
+    public $_configuration = array(
+        'NameID'                => '',
+        'Issuer'                => '',
+        'IDPMetadataUrl'        => '',
+        'SPReturnUrl'           => '',
+        'SigningCertificate'    => '',
+        'EncryptionCertificate' => ''
+    );
+
+    /**
+     * Set the configuration for the SAML Connection
+     *
+     * @param array $_configuration
+     */
+    public function __construct($_configuration = array())
+    {
+        $this->setConfiguration($_configuration);
+    }
+
+    /**
+     * Set the field that is used as NameId
+     *
+     * example: urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress
+     *
+     * @param string $nameId
+     */
+    public function setNameId($nameIdField)
+    {
+        $this->_configuration['NameID'] = $nameIdField;
+    }
+
+    /**
+     * Get the field that is used as NameId
+     *
+     * @return string
+     */
+    public function getNameId()
+    {
+        return $this->_configuration['NameID'];
+    }
+
+    /**
+     * Get the issuer
+     *
+     * @return string
+     */
+    public function getIssuer()
+    {
+        return $this->_configuration['Issuer'];
+    }
+
+    /**
+     * Set the name of the SP
+     *
+     * @return string name of the SP
+     */
+    public function setIssuer($issuer = '')
+    {
+        $this->_configuration['Issuer'] = $issuer;
+    }
+
+    /**
+     * Set the IDP Metadata URL
+     *
+     * @param string $idpMetadataUrl
+     */
+    public function setIdpMetadataUrl($idpMetadataUrl)
+    {
+        $this->_configuration['IDPMetadataUrl'] = $idpMetadataUrl;
+    }
+
+    /**
+     * Get the IDP Metadata URL
+     * @return string
+     */
+    public function getIdpMetadataUrl()
+    {
+        return $this->_configuration['IDPMetadataUrl'];
+    }
+
+    /**
+     * Set the return URL to return to in the SP
+     * @param string $spReturnUrl
+     */
+    public function setSpReturnUrl($spReturnUrl = '')
+    {
+        $this->_configuration['SPReturnUrl'] = $spReturnUrl;
+    }
+
+    /**
+     * Get the SP return URL
+     *
+     * @return string
+     */
+    public function getSpReturnUrl()
+    {
+        return $this->_configuration['SPReturnUrl'];
+    }
+
+    /**
+     * @param object Wizkunde\OpenSAML\Configuration\Certificate
+     */
+    public function setIdpSigningCertificate(Certificate $certificate)
+    {
+        $this->_configuration['SigningCertificate'] = $certificate;
+    }
+
+    /**
+     * @return object Wizkunde\OpenSAML\Configuration\Certificate
+     */
+    public function getIdpSigningCertificate()
+    {
+        return $this->_configuration['SigningCertificate'];
+    }
+
+    /**
+     * @param object Wizkunde\OpenSAML\Configuration\Certificate
+     */
+    public function setIdpEncryptionCertificate(Certificate $certificate)
+    {
+        $this->_configuration['EncryptionCertificate'] = $certificate;
+    }
+
+    /**
+     * @return object Wizkunde\OpenSAML\Configuration\Certificate
+     */
+    public function getIdpEncryptionCertificate()
+    {
+        return $this->_configuration['EncryptionCertificate'];
+    }
+
+    /**
+     * Get the Configuration data
+     *
+     * @return mixed array with configuraion data
+     */
+    public function getConfiguration()
+    {
+        return $this->_configuration;
+    }
+
+    /**
+     * @param mixed $configurationData configuration data
+     */
+    public function setConfiguration(array $configurationData)
+    {
+        if(count($configurationData) > 0) {
+            foreach($configurationData as $configurationKey => $value)
+            {
+                if(array_key_exists($configurationData, $this->_configuration)) {
+                    $this->_configuration[$configurationData] = $value;
+                }
+            }
+        }
+    }
+}
